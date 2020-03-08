a91d14b2e501e2453cc150219d4f36f19a656bfc
Jérémy Lal <kapouer@melix.org>
2020-03-08 15:08:16 +0100

    Move things around

---
+++ bin/uncss-native.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+const getopts = require("getopts");
+const Uncss = require('../');
+const { readFile } = require('fs').promises;
+
+
+const opts = getopts(process.argv.slice(2), {
+	string: ['output'],
+	boolean: ['links', 'forms'],
+	default: {
+		output: 'output'
+	}
+});
+
+async function sitemapList(loc) {
+	const buf = await readFile(loc);
+	return buf.toString().split('\n').filter(line => line.length);
+}
+
+(async () => {
+	const list = [];
+	for (const loc of opts._) {
+		if (/^https?:/.test(loc)) {
+			list.push(loc);
+		} else {
+			list.push(... await sitemapList(loc));
+		}
+	}
+	delete opts._;
+
+	await Uncss.run(list, Object.assign({}, opts));
+})();
+
--- index.js
+++ index.js
@@ -1,32 +1,77 @@
-const getopts = require("getopts");
-const coverage = require('./coverage');
-const { readFile } = require('fs').promises;
+const { chromium } = require('playwright-chromium');
+const fs = require('fs').promises;
+const makeDir = require('make-dir');
+const URL = require('url');
+const Path = require('path');
 
+exports.run = async function(urls, opts) {
+	const cov = await exports.coverage(urls, opts);
+	await exports.write(opts.output, cov);
+};
 
-const opts = getopts(process.argv.slice(2), {
-	string: ['output'],
-	boolean: ['links', 'forms'],
-	default: {
-		output: 'output'
-	}
-});
+exports.coverage = async function(list, opts) {
+	const browser = await chromium.launch({
+		// dumpio: true
+	});
+	const context = await browser.newContext({
+		ignoreHTTPSErrors: true,
+		bypassCSP: true
+	});
+	const page = await context.newPage();
+	
+	await page.coverage.startCSSCoverage({
+		resetOnNavigation: false
+	});
+	
+	await prepare(page);
+	
+	const visits = {};
+	await crawl(page, list, visits, opts);
 
-async function sitemapList(loc) {
-	const buf = await readFile(loc);
-	return buf.toString().split('\n').filter(line => line.length);
+	const cov = await page.coverage.stopCSSCoverage();
+	browser.close();
+	return cov;
 }
 
-(async () => {
-	const list = [];
-	for (const loc of opts._) {
-		if (/^https?:/.test(loc)) {
-			list.push(loc);
-		} else {
-			list.push(... await sitemapList(loc));
-		}
+exports.write = async function(output, cov) {
+	await Promise.all(cov.map(async obj => {
+		await writeFile(output, obj);
+	}));
+};
+
+async function prepare(page) {
+	// await page.route('**/*.{png,jpg,jpeg,mp4,webp,webm,gif,svg}', request => request.abort());
+	await page.evaluate(function() {
+		Object.defineProperty(document, "visibilityState", {
+			configurable: true,
+			get: function() { return "prerender"; }
+		});
+	});
+}
+
+async function crawl(page, list=[], visits, opts) {
+	for (const url of list) {
+		if (visits[url]) continue;
+		visits[url] = true;
+		await page.goto(url);
+		const obj = await page.evaluate(function(opts) {
+			const obj = {};
+			if (opts.links) obj.links = document.links.filter(link => {
+				return link.hostname == document.location.hostname;
+			}).map(link => link.href);
+			return obj;
+		}, opts);	
+		await crawl(page, obj.links, visits, opts);
 	}
-	delete opts._;
+}
 
-	await coverage.run(list, Object.assign({}, opts));
-})();
-
+async function writeFile(output, item) {
+	const pathname = URL.parse(item.url).pathname;
+	if (!pathname || pathname == "/") return;
+	const filePath = Path.join(output, pathname);
+	var list = item.ranges.map((range) => {
+		return item.text.slice(range.start, range.end);
+	});
+	await makeDir(Path.dirname(filePath));
+	await fs.writeFile(filePath, list.join('\n'));
+}
